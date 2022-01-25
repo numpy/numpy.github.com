5710ef063e76cdbd6abd624212ab275bab5fac83
jarrodmillman <jarrodmillman@users.noreply.github.com>
2022-01-25 19:21:43 +0000

    Publishing Site numpy.org at 390cbf61fda5b2a39130d0b2de7aaed13ba0d00e on Tue Jan 25 19:21:43 UTC 2022

--- js/thebe.js
+++
@@ -1,9 +0,0 @@
-<!-- Thebe Configuration -->
-<script type="text/x-thebe-config">
-    {
-        requestKernel: true,
-        binderOptions: {
-            repo: "joelachance/thebelab-requirements"
-        },
-    }
-</script>
